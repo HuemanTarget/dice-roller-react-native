e39c7adfbe7e1485a67027c02b8988312cf8732a
PompousPro <pompouspro@PompousPros-MacBook-Pro.local>
2020-06-24 12:43:34 -0700

    add touchable opactiy button

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity, Alert } from "react-native";
 
 export default function App() {
+
+  playButtonPressed = () => {
+    Alert.alert('button worked')
+  }
+
   return (
     <View style={styles.container}>
       <Image source={require("./src/images/dice1.png")} />
+      <TouchableOpacity onPress={playButtonPressed}>
+        <Text style={styles.gamebutton}>Play Game</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -15,5 +23,16 @@ const styles = StyleSheet.create({
     backgroundColor: "#E74292",
     alignItems: "center",
     justifyContent: "center",
+  },
+  gamebutton: {
+    marginTop: 35,
+    fontSize: 20,
+    color: "white",
+    fontWeight: "bold",
+    borderWidth: 2,
+    paddingVertical: 8,
+    paddingHorizontal: 40,
+    borderRadius: 5,
+    borderColor: "white",
   },
 });
